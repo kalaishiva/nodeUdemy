c932c0907b0770de1209efc7dc042caa9fb2c4ec
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-06-22 00:27:12 +0200

    learned the npm package and its various use cases

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World !");
+let superheroes = require('superheroes');
+let supervillains = require('supervillains');
+
+let mySuperHeroeName = superheroes.random();
+console.log(mySuperHeroeName);
+
+let mySupervillanName = supervillains.random();
+console.log(mySupervillanName);
